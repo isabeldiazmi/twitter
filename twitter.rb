b7dacfe375512170ccc758f108aabe18a284ac0f
codeamac07 <codeamac07@gmail.com>
2015-10-28 09:58:43 -0600

    Crea clase con metodo para parsear archivos html y extrae el username de la pagina

---
+++ twitter.rb
@@ -0,0 +1,29 @@
+require "nokogiri"
+require "open-uri"
+
+class TwitterScrapper
+  attr_accessor :doc
+  
+  def initialize(url)
+    @doc = Nokogiri::HTML(File.open(url))
+  end
+
+  def interfaz
+  end
+
+  def extract_username
+    profile_name = doc.search(".ProfileHeaderCard-name > a")
+    profile_name.first.inner_text
+  end
+
+  def extract_tweets
+  end
+
+  def extract_stats
+  end
+
+end
+
+twitter = TwitterScrapper.new('twitter_account.html')
+#puts twitter.doc
+puts twitter.extract_username
