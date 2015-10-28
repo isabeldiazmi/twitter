4b586f12b1349e81b0adbc94288d2b2cec5b311d
codeamac07 <codeamac07@gmail.com>
2015-10-28 10:33:02 -0600

    extract_stats a traves del doc.search extrae la informacion del html usando la jerarquia de css

--- twitter.rb
+++ twitter.rb
@@ -6,18 +6,20 @@ class TwitterScrapper
   
   def initialize(url)
     @doc = Nokogiri::HTML(File.open(url))
+    @arr_stats = []
   end
 
   def interfaz
+    puts "---------------------------------------------------"
     puts "Username: #{extract_username}"
     puts "---------------------------------------------------"
-    puts "STATS -> Tweets: Siguiendo: Seguidores: Favoritos:"
+    puts "STATS -> Tweets: #{} Siguiendo: Seguidores: Favoritos:"
     puts "---------------------------------------------------"
     puts "Tweets:"
   end
 
   def extract_username
-    profile_name = doc.search(".ProfileHeaderCard-name > a")
+    profile_name = @doc.search(".ProfileHeaderCard-name > a")
     profile_name.first.inner_text
   end
 
@@ -25,10 +27,21 @@ class TwitterScrapper
   end
 
   def extract_stats
+    no_tweets = @doc.search(".ProfileNav-list > .ProfileNav-item:nth-child(1) > .ProfileNav-stat > span:nth-child(2)")
+    @arr_stats << no_tweets.inner_text
+    siguiendo = @doc.search(".ProfileNav-list > .ProfileNav-item:nth-child(2) > .ProfileNav-stat > span:nth-child(2)")
+    @arr_stats << siguiendo.inner_text
+    seguidores = @doc.search(".ProfileNav-list > .ProfileNav-item:nth-child(3) > .ProfileNav-stat > span:nth-child(2)")
+    @arr_stats << seguidores.inner_text
+    favoritos = @doc.search(".ProfileNav-list > .ProfileNav-item:nth-child(4) > .ProfileNav-stat > span:nth-child(2)")
+    @arr_stats << favoritos.inner_text
+    p @arr_stats
   end
 
 end
 
 twitter = TwitterScrapper.new('twitter_account.html')
 #puts twitter.doc
-puts twitter.extract_username
+#puts twitter.extract_username
+twitter.interfaz
+twitter.extract_stats
